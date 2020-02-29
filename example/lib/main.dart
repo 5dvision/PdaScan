ef17e2d185ea9db9e60d4ab32e4236c5eb30daa0
luogang <luogang@missfresh.cn>
2020-02-29 14:29:13 +0800

    fix:APP进入后，关掉系统的键盘输入模式，退出回复

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 import 'package:pda_scan/pda_scan.dart';
+import 'package:pda_scan_example/test.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,62 +11,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String result = " 没有显示内容";
 
-  String result =" 没有显示内容";
+  PdaScan pdaScan;
 
   @override
   void initState() {
     super.initState();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-          children: <Widget>[
-          FlatButton(child: Text('扫描'),onPressed: (){
-          initScan();
-           },),
-          Text(result)
-  ],
-  )
-        ),
+          appBar: AppBar(
+            title: const Text('Plugin example app'),
+          ),
+          body: Column(
+            children: <Widget>[
+              SearchEdit(controller: TextEditingController(),
+                  hintText: '输入', scanCallback: ([r, e]) {
+                    print('返回结果${r.toString()}|| ${e.toString()}');
+                  }),
+
+            ],
+          )
       ),
     );
   }
 
-  Future<void> initScan() async {
-    var  isPDA = await PdaScan.isPDA;
-    if(isPDA == true){
-      var pdaScan = PdaScan();
-      pdaScan.onScanResult.listen(_onEvent, onError: _onError);
-    }else{
-      String content = await PdaScan.scanResult;
-      print("扫描结果是：" + content);
-      setState(() {
-        result  = content;
-      });
-    }
-  }
-
-  void _onEvent(Object event) {
-    print("返回成功的数据"+event.toString());
-    print("PDA 结果是：" + event);
-    setState(() {
-      result  = event;
-    });
-  }
-  void _onError(Object error) {
-    print("扫描错误的信息"+error.toString());
-  }
 }
-
-
-
-
